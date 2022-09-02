e703d2772e8e50f497ce9e8597fdc3886add8a46
Rodrigo Diaz Regis <rodridiazregis@hotmail.com>
2022-09-01 21:40:57 -0300

    modelos order y bills

---
+++ models/Bill.js
@@ -0,0 +1,61 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('bill',
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV1,
+            primaryKey: true,
+            allowNull: false
+        },
+        billNumber: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            default: 1
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: {
+                    msg: 'El email no es válido',
+                    args: true
+                }
+            }
+        },
+        adress: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        celNumber: {
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        buyersId: {
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        totalAmount: {
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        observations: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        }
+    },  
+    {
+        timestamps: false
+    });
+}; 
+
