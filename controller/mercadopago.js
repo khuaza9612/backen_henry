e898be30a29542ad20e6a5c9ac8a62e65a148bf7
khuaza9612 <102488101+kevinhuaza@users.noreply.github.com>
2022-09-11 14:26:17 -0500

    mercado-pago

--- controller/mercadopago.js
+++ controller/mercadopago.js
@@ -8,6 +8,7 @@ const crearOrdem = async (req, res) => {
 mercadopago.configure({
     access_token: 'TEST-7297348892296136-090721-aa4217832aaed5215f291f69e9f6acd8-1194321094'
 });
+const {price}=req.body;
 
 let preference = {
     items: [
@@ -15,19 +16,20 @@ let preference = {
             title: 'test',
             quantity:1,
             currency_id: 'ARS',
-            unit_price: 1.5
+            unit_price: price
+
             
         }
     ],
-    notification_url: "",
+    notification_url: "www.",
 
 };
 
 mercadopago.preferences.create(preference)
 .then(function(response){
-    console.log(response.body);
+    res.json(response.body);
 }).catch(function(error){
-    console.log(error);
+    //console.log(error);
 });
 }
 const notificacionOrden = async(req, res) => {
