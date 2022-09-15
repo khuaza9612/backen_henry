63ace1eec9658d2647c31c3b495e8b6d8401935d
khuaza9612 <andresnavia-96@hotmail.com>
2022-09-14 21:29:25 -0500

    carga de zise

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use('/api', require('./routes'));
 
 conn.sync({ force: true }).then(() => {
-  app.listen(false, () => {
-   //LoadDb();
+  app.listen(3001, () => {
+   LoadDb();
     console.log('✓ Se conectó a la base de datos');
   })
 });
