d02baed72503d49bd78a92ad8970fa154e0eda36
khuaza9612 <andresnavia-96@hotmail.com>
2022-09-20 18:57:18 -0500

    email full

--- controller/mercadopago.js
+++ controller/mercadopago.js
@@ -31,7 +31,7 @@ let preference = {
             installments: 6, //cantidad maximo de cuotas
         },
         back_urls:{
-             success: "https://athens-theta.vercel.app/purchase",
+             success: "http://localhost:3000/",
             failure: "https://athens-5qhdwams0-ingmsaac-gmailcom.vercel.app/",
             pending: "https://athens-5qhdwams0-ingmsaac-gmailcom.vercel.app/",
         }
